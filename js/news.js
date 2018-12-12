a72751ba293a11ed75a51a9ab2eda0aa70fe9c80
uncleNAO <admin@naozhendang.com>
2018-12-12 12:04:47 +0800

    update json data file
    rt

--- js/news.js
+++ js/news.js
@@ -24,7 +24,7 @@ $(function(){
 
 
     function n2o(){
-      $.getJSON('https://www.poisonmushroom.cc/js/news.json', function(data) {
+      $.getJSON('https://www.poisonmushroom.cc/news.json', function(data) {
 
         var items = data.news;
 
